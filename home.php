6791a3142f594a582cc7f7c7ce9a3e072d4189ed
Daniel Victor <danielvictor.oq92@yahoo.com.br>
2020-11-25 21:44:07 -0300

    Ajustes

--- home.php
+++ home.php
@@ -3,6 +3,9 @@ require_once('jogos.php');
 
 $jogos = new Jogos;
 
-$jogos->setQtdDezenas(8);
+// $jogos->setQtdDezenas(8);
+// $jogos->setTotalJogos(5);
+// $jogos->setJogos(5);
+// $jogos->setResultado([1,2,3]);
 
 echo $jogos->html();
